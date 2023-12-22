86c6d32a0e56a6a1f52f05af92777f967c3934c5
Lauren <43707274+Lorn12@users.noreply.github.com>
2023-12-21 18:15:48 -0600

    Implemented banner component/heading rotation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { NavBar } from "./components/NavBar.js";
+import { Banner } from "./components/Banner.js";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 }
