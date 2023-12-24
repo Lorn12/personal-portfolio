f42cfd39c7a11cd3660f495e4e655a8b802e9a6a
Lauren <43707274+Lorn12@users.noreply.github.com>
2023-12-23 19:53:41 -0600

    Updated Contact.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { NavBar } from "./components/NavBar.js";
 import { Banner } from "./components/Banner.js";
 import { Skills } from "./components/Skills.js";
 import { Projects } from "./components/Projects.js";
+import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer.js";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -13,6 +14,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Contact />
       <Footer />
     </div>
   );
