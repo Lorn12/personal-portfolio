1ba73e9a13dfd094a653f91061e0e2045dddea68
Lauren <43707274+Lorn12@users.noreply.github.com>
2023-12-23 19:16:00 -0600

    implemented footer section/updated errors/styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { NavBar } from "./components/NavBar.js";
 import { Banner } from "./components/Banner.js";
 import { Skills } from "./components/Skills.js";
 import { Projects } from "./components/Projects.js";
-import { Contact } from "./components/Contact.js";
+import { Footer } from "./components/Footer.js";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
-      <Contact />
+      <Footer />
     </div>
   );
 }
--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,14 +1,14 @@
-import { Alert } from "react-bootstrap";
 import { useState, useEffect } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
 
-export const Newsletter = ({ onValidated, status, message }) => {
+export const Newsletter = ({ status, message, onValidated }) => {
   const [email, setEmail] = useState("");
 
   useEffect(() => {
     if (status === "success") clearFields();
   }, [status]);
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     email &&
       email.indexOf("@") > -1 &&
@@ -23,10 +23,12 @@ export const Newsletter = ({ onValidated, status, message }) => {
 
   return (
     <Col lg={12}>
-      <div className="newsletter-bx">
+      <div className="newsletter-bx wow slideInUp">
         <Row>
           <Col lg={12} md={6} xl={5}>
-            <h3>Subscribe to our Newsletter</h3>
+            <h3>
+              Subscribe to our Newsletter<br></br> & Never miss latest updates
+            </h3>
             {status === "sending" && <Alert>Sending...</Alert>}
             {status === "error" && <Alert variant="danger">{message}</Alert>}
             {status === "success" && <Alert variant="success">{message}</Alert>}
